7e922e2fe79d996ce1351acb4b5d3ad2f7a12e0f
Kevin Inzelrac <kevininzelrac@me.com>
2023-11-29 02:50:43 +0100

    add Home page

---
+++ app/routes/_index/route.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <main>
+      <p>Remix app deployed on Vercel with Prisma PostreSQL Neon Database</p>
+    </main>
+  );
+}
