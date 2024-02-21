4e4dd04a3c40ea84330c58dd3d741fa77eda5f75
Kevin Inzelrac <kevininzelrac@me.com>
2024-02-21 01:32:12 +0100

    core updates

--- remix.config.js
+++ remix.config.js
@@ -5,5 +5,26 @@ export default {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
-  serverDependenciesToBundle: [/react-icons*/],
+  serverDependenciesToBundle: [
+    /react-icons*/,
+    //
+    "slate",
+    "slate-history",
+    "slate-react",
+    "direction",
+    /lodash*/,
+    "scroll-into-view-if-needed",
+    "compute-scroll-into-view",
+    "is-plain-object",
+    "immer",
+    "@juggle/resize-observer",
+    "is-hotkey",
+    //
+    //"react",
+    //"react-dom",
+    //"scheduler",
+    //
+    "bcryptjs",
+    //
+  ],
 };
--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,2 +1,32 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node" />
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    DATABASE_URL: string;
+    DIRECT_URL: string;
+
+    PWD_HASH: string;
+
+    SESSION_SECRET: string;
+    ACCESS_SECRET: string;
+    REFRESH_SECRET: string;
+    ACCESS_TOKEN_DURATION: string;
+
+    BLOB_READ_WRITE_TOKEN: string;
+
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    GOOGLE_REDIRECT_URL: string;
+
+    GMAIL_USER: string;
+    GMAIL_SECRET: string;
+
+    RECAPTCHA_PROJECT_ID: string;
+    RECAPTCHA_SITE_KEY: string;
+    GOOGLE_API_KEY: string;
+
+    HELLO_ASSO_CLIENT_ID: string;
+    HELLO_ASSO_CLIENT_SECRET: string;
+  }
+}
