552123d0389c73f0f54fa05aab72faa290899f17
Kevin Inzelrac <kevininzelrac@me.com>
2024-03-18 19:56:50 +0100

    improving routes/_auth

--- app/routes/_auth/route.tsx
+++ app/routes/_auth/route.tsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet } from "@remix-run/react";
+import { NavLink, Outlet, useLocation } from "@remix-run/react";
 import { LinksFunction } from "@remix-run/node";
 
 import loader from "./loader";
@@ -6,16 +6,22 @@ import ErrorBoundary from "~/components/errorBoundary";
 export { loader, ErrorBoundary };
 
 import styles from "./styles.css";
+import Transition from "~/components/transition";
 export let links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function Auth() {
+  const { key } = useLocation();
   return (
-    <main>
-      <nav>
-        <NavLink to="signin">Sign In</NavLink>•
-        <NavLink to="signup">Sign Up</NavLink>
-      </nav>
-      <Outlet />
-    </main>
+    <Transition key={key}>
+      <main>
+        <article>
+          <nav>
+            <NavLink to="signin">Sign In</NavLink>•
+            <NavLink to="signup">Sign Up</NavLink>
+          </nav>
+          <Outlet />
+        </article>
+      </main>
+    </Transition>
   );
 }
