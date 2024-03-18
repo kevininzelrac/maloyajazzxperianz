3ba7516a9c8d5d75b4f80bbd07a9952855fd8990
Kevin Inzelrac <kevininzelrac@me.com>
2024-03-18 19:45:47 +0100

    add utils/index

---
+++ app/utils/index.ts
@@ -0,0 +1,11 @@
+import sleep from "./sleep";
+import ClientOnly from "./clientOnly";
+import importScript from "./importScript";
+import convertFormData from "./convertFormData";
+
+export default {
+  sleep,
+  ClientOnly,
+  importScript,
+  convertFormData,
+};
