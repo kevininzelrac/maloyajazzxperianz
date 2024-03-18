59ef65f5315ba62e1fe91c981da813ad69114be3
Kevin Inzelrac <kevininzelrac@me.com>
2024-03-18 19:53:04 +0100

    update components/badge

--- app/components/badge.tsx
+++ app/components/badge.tsx
@@ -1,35 +1,22 @@
 import { Prisma } from "@prisma/client";
 import { SerializeFrom } from "@remix-run/node";
-import { CiCalendar } from "react-icons/ci";
 
 export default function Badge({
-  createdAt,
   author,
 }: SerializeFrom<
   Prisma.PostGetPayload<{
     select: {
-      createdAt: true;
-      author: { select: { firstname: true; avatar: true } };
+      author: { select: { firstname: true; lastname: true; avatar: true } };
     };
   }>
 >) {
   return (
     <div className="badge">
       <img src={author?.avatar} width={30} height={30} />
-      <span>
-        <span>
-          <strong>{author?.firstname}</strong>
-          <small>
-            <i> • job todo...</i>
-          </small>
-        </span>
-        {createdAt ? (
-          <time>
-            <CiCalendar />
-            {new Date(createdAt!).toLocaleDateString("fr")}
-          </time>
-        ) : null}
-      </span>
+      <div>
+        <strong>{author?.firstname}</strong>
+        <small>{author?.lastname}</small>
+      </div>
     </div>
   );
 }
