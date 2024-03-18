40a8a3c2903b5d9059ddc775bf39f9f5f3c7b9ae
Kevin Inzelrac <kevininzelrac@me.com>
2024-03-18 19:47:53 +0100

    add db/index

---
+++ app/db/index.ts
@@ -0,0 +1,11 @@
+import find from "./find";
+import create from "./create";
+import update from "./update";
+import _delete from "./delete";
+
+export default {
+  find,
+  create,
+  update,
+  delete: _delete,
+};
