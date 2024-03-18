5457987bf0478796a9afd714b07a580ba5eb2c8a
Kevin Inzelrac <kevininzelrac@me.com>
2024-03-18 21:25:21 +0100

    update root

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,3 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import {
-  json,
-  type LinksFunction,
-  type LoaderFunctionArgs,
-} from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -15,92 +9,17 @@ import {
 } from "@remix-run/react";
 
 import { IconContext } from "react-icons";
-import auth from "./services/auth.server";
-import prisma from "./services/prisma.server";
-
-import Nav from "./components/nav";
-import Footer from "./components/footer";
+import { Franklin } from "./svg";
 import backgroundImage from "../public/img/emy05.jpg";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
-import a from "./styles/a.css";
-import animations from "./styles/animations.css";
-import badge from "./styles/badge.css";
-import borders from "./styles/borders.css";
-import dialog from "./styles/dialog.css";
-import fonts from "./styles/fonts.css";
-import footer from "./styles/footer.css";
-import nav from "./styles/nav.css";
-import root from "./styles/root.css";
-import variables from "./styles/variables.css";
-import loading from "./styles/loading.css";
-import withPriviledges from "./middlewares/withPriviledge";
-
-export const links: LinksFunction = () => [
-  ...(cssBundleHref
-    ? [{ rel: "stylesheet", href: cssBundleHref }]
-    : [
-        { rel: "stylesheet", href: a },
-        { rel: "stylesheet", href: animations },
-        { rel: "stylesheet", href: badge },
-        { rel: "stylesheet", href: borders },
-        { rel: "stylesheet", href: dialog },
-        { rel: "stylesheet", href: fonts },
-        { rel: "stylesheet", href: footer },
-        { rel: "stylesheet", href: nav },
-        { rel: "stylesheet", href: root },
-        { rel: "stylesheet", href: loading },
-        { rel: "stylesheet", href: variables },
-      ]),
-];
+import links from "./styles/index";
 
 import loader from "./loader";
-export { loader };
-// export const loader = async ({ request }: LoaderFunctionArgs) => {
-//   const { id, headers } = await auth(request);
+export { loader, links };
 
-//   const user = id
-//     ? await prisma.user.findUnique({
-//         where: {
-//           id,
-//         },
-//         select: {
-//           id: true,
-//           firstname: true,
-//           lastname: true,
-//           avatar: true,
-//           role: true,
-//         },
-//       })
-//     : null;
-
-//   const nav = await prisma.post.findMany({
-//     where: withPriviledges(user, {
-//       NOT: { OR: [{ type: "post" }, { category: "post" }] },
-//     }),
-//     select: {
-//       title: true,
-//       type: true,
-//       category: true,
-//     },
-//     orderBy: { createdAt: "asc" },
-//   });
-
-//   const recursive = (data: any, parent: string) =>
-//     data
-//       .filter(({ category }: any) => category === parent)
-//       .map(({ title, type, category }: any) => ({
-//         title,
-//         type,
-//         category,
-//         children: recursive(data, title),
-//       }));
-
-//   return json({ id, user, nav: recursive(nav, "page") }, { headers });
-// };
-
-export default function App() {
-  const { user, nav } = useLoaderData<typeof loader>();
-
+function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
@@ -110,22 +29,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <img src={backgroundImage} className="background-image" />
-        <h1>
-          Maloya <br />
-          ---Jazz <br />
-          Xperianz
-        </h1>
-        <Nav user={user} data={nav} />
-
-        <IconContext.Provider
-          value={{
-            className: "react-icons",
-          }}
-        >
-          <Outlet />
-          <Footer />
-        </IconContext.Provider>
+        {children}
         <ScrollRestoration
           getKey={(location, matches) => {
             return ["blog"].includes(location.pathname)
@@ -137,5 +41,25 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  const { user, nav } = useLoaderData<typeof loader>();
+
+  return (
+    <Layout>
+      <IconContext.Provider
+        value={{
+          className: "react-icons",
+        }}
+      >
+        <Header user={user} data={nav} />
+        <Outlet />
+        <Footer />
+        <img src={backgroundImage} className="background-image" />
+        <Franklin className="franklin" />
+      </IconContext.Provider>
+    </Layout>
   );
 }
