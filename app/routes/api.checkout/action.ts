985810b18ffd634756153d84fc8dd51ff230a858
Kevin Inzelrac <kevininzelrac@me.com>
2024-03-18 21:10:55 +0100

    update routes/api.checkout

--- app/routes/api.checkout/action.ts
+++ app/routes/api.checkout/action.ts
@@ -1,4 +1,4 @@
-import { LoaderFunctionArgs, json, redirect } from "@remix-run/node";
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import auth from "~/services/auth.server";
 import getToken from "./getToken";
 import checkout from "./checkout";
@@ -6,25 +6,19 @@ import checkout from "./checkout";
 export const action = async ({ request }: LoaderFunctionArgs) => {
   const { firstname, lastname, email } = await auth(request);
   const formData = await request.formData();
-  const item = formData.get("item") as string;
-  const amount = formData.get("amount") as string;
 
   const { access_token } = await getToken();
-  //console.log(access_token);
 
   const response = await checkout({
     access_token: access_token,
-    item,
-    amount: parseInt(amount),
+    item: String(formData.get("item")),
+    amount: Number(formData.get("amount")),
     firstname,
     lastname,
     email,
   });
-  //console.log(response);
 
-  //return json({ response });
   return redirect(response.redirectUrl);
-  //return null;
 };
 
 export default action;
