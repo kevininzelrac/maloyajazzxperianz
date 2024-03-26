ed1159aa5d49c3f2b1a8a2f7fc0890d52e2dfdd1
Kevin Inzelrac <kevininzelrac@me.com>
2024-03-26 23:43:08 +0100

    updating routes/settings

--- app/routes/settings/loader.ts
+++ app/routes/settings/loader.ts
@@ -1,24 +1,55 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
-import db from "~/db";
 import auth from "~/services/auth.server";
+import prisma from "~/services/prisma.server";
 
 const loader = async ({ request }: LoaderFunctionArgs) => {
   const { id, headers } = await auth(request);
-  return json(
-    {
-      user: await db.find.user({
-        where: { id },
-        select: {
-          id: true,
-          email: true,
-          firstname: true,
-          lastname: true,
-          role: true,
-          avatar: true,
-        },
-      }),
+  if (!id) throw json(null, { status: 401, statusText: "Unauthorized" });
+
+  const user = await prisma.user.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      email: true,
+      firstname: true,
+      lastname: true,
+      role: true,
+      avatar: true,
     },
-    { headers }
-  );
+  });
+  if (!user) throw json(null, { status: 404, statusText: "Not Found" });
+
+  return json({ user }, { headers });
 };
 export default loader;
+
+// import { LoaderFunctionArgs, json } from "@remix-run/node";
+// import withTryCatch from "~/middlewares/withTryCatch";
+// import auth from "~/services/auth.server";
+// import prisma from "~/services/prisma.server";
+
+// const loader = async ({ request }: LoaderFunctionArgs) => {
+//   const { id, headers } = await auth(request);
+//   if (!id) throw json(null, { status: 401, statusText: "Unauthorized" });
+
+//   const user = await withTryCatch(
+//     prisma.user.findUnique({
+//       where: { id },
+//       select: {
+//         id: true,
+//         email: true,
+//         firstname: true,
+//         lastname: true,
+//         role: true,
+//         avatar: true,
+//       },
+//     }),
+//     "Failed to load user"
+//   );
+//   if (user.error)
+//     throw json(user, { status: 500, statusText: "Internal Server Error" });
+//   if (!user.data) throw json(null, { status: 401, statusText: "Unauthorized" });
+
+//   return json({ user: user.data }, { headers });
+// };
+// export default loader;
--- app/routes/settings/route.tsx
+++ app/routes/settings/route.tsx
@@ -1,33 +1,32 @@
 import { LinksFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+import Badge from "~/components/badge";
+import Transition from "~/components/transition";
 
 import styles from "./styles.css";
 export let links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 import loader from "./loader";
-import { useLoaderData } from "@remix-run/react";
-import Badge from "~/components/badge";
-import Error from "~/components/error";
-export { loader };
+import ErrorBoundary from "~/components/errorBoundary";
+export { loader, ErrorBoundary };
 
 export default function Settings() {
   const { user } = useLoaderData<typeof loader>();
   return (
-    <main>
-      <article>
-        <h3>Settings</h3>
-        <Error data={user}>
-          {(data) => (
-            <>
-              <Badge author={data!} />
-              <p>{user.data?.role}</p>
-            </>
-          )}
-        </Error>
-        <h3>TODO</h3>
-        <p>update avatar</p>
-        <p>update password</p>
-        <p>delete account</p>
-      </article>
-    </main>
+    <Transition>
+      <main>
+        <article>
+          <h3>Settings</h3>
+          <Badge author={user} />
+          <p>{user.role}</p>
+
+          <h3>TODO</h3>
+          <p>update avatar</p>
+          <p>update password</p>
+          <p>delete account</p>
+        </article>
+      </main>
+    </Transition>
   );
 }
